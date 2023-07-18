10ff11d7782a8273b33ad675f9becddc8c4e1522
nicole tran <nicolet2090@gmail.com>
2023-07-18 10:21:15 -0700

    Initial Commit

---
+++ toDoListTake2/toDoListTake2App.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListTake2App.swift
+//  toDoListTake2
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListTake2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
